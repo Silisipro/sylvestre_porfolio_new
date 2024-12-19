e45966a680e74fb81bae0eef2d4991266e3eed77
unknown <silisipro@gmail.com>
2024-12-19 10:00:21 +0100

    maj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,22 +16,32 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <p>
+            Visitez quelques unes de mes réalisations
+          </p>
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://louermeublee.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            
-            visite une de mes réalisations
+            Louer Meublee
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://app.quotidishop.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Quotidishop app
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="https://quotidishop.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Quotidishop boutique
           </a>
         </div>
       </main>
